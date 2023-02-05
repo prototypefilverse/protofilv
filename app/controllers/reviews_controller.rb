0fc4e9211eac63f3427ca907902846b164e89282
prototypefilverse <morikawasusumu.b@gmail.com>
2023-02-05 10:00:37 +0900

    レビューフォーマット雛形

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,26 @@
 class ReviewsController < ApplicationController
+
+  def new
+    @movie = Movie.find(params[:movie_id])
+    @review = @movie.reviews.new
+  end
+
+  def create
+    @movie = Movie.find(params[:movie_id])
+    @review = @movie.reviews.new(review_params)
+    @review.reviewer = current_user
+    if @review.save
+      redirect_to movie_path(@movie), notice: "レビューが投稿されました"
+    else
+      render :new
+    end
+
+  end
+
+  private
+  
+  def review_params
+   params.require(:review).permit(:content)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'filvs#index'
   resources :filvs, only: :index
-  
+
+  resources :movies do
+    resources :reviews, only: [:new, :create]
+    
+  end
+
+
 end
