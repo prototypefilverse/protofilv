2c72bfaef470f1c9e34cec8463d6aad640087369
prototypefilverse <morikawasusumu.b@gmail.com>
2023-02-09 02:14:29 +0900

    Update filvs_controller.rb

--- app/controllers/filvs_controller.rb
+++ app/controllers/filvs_controller.rb
@@ -1,6 +1,7 @@
 class FilvsController < ApplicationController
 
   def index
+
     if params[:movie_id].present?
       @movie = Movie.find(params[:movie_id]) 
       @reviews = @movie.reviews
@@ -9,6 +10,7 @@ class FilvsController < ApplicationController
     if user_signed_in?
       @filv = current_user.filv
     end
+    
   end
 
 end
