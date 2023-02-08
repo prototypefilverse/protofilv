e5cec866bd068e04a0985eff1e6e501f1279bf79
prototypefilverse <morikawasusumu.b@gmail.com>
2023-02-09 00:26:28 +0900

    トップから映画検索から一覧表示機能実装

--- app/controllers/filvs_controller.rb
+++ app/controllers/filvs_controller.rb
@@ -1,7 +1,7 @@
 class FilvsController < ApplicationController
 
     def index
-      @movie = Movie.find(1)  
+      @movie = Movie.find(params[:movie_id]) 
       @reviews = @movie.reviews
      if user_signed_in?
       @filv = current_user.filv
