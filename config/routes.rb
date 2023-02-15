bea42a0aa36f72f4f0cb36cc7cba6b2e26dde32a
prototypefilverse <morikawasusumu.b@gmail.com>
2023-02-15 21:26:26 +0900

    レビュー評価ボタンとDBへの保存実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,12 @@ Rails.application.routes.draw do
   root to: 'filvs#index'
   resources :filvs, only: :index
   resources :reviewers, only: [:index, :destroy]
+
   resources :movies, only: [:index] do
     resources :filvs, only: [:index, :new, :create]
-    resources :reviews, only: [:new, :create]
+    resources :reviews, only: [:new, :create] do
+      resources :ratings, only: [:create]
+    end  
   end
   
 end
