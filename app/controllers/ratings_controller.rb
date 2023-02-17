7b424488934551f0c0dcc4eab009ef9078cec8f7
prototypefilverse <morikawasusumu.b@gmail.com>
2023-02-17 10:19:49 +0900

    途中

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -2,18 +2,14 @@ class RatingsController < ApplicationController
 
   def create
     @review = Review.find(params[:review_id])
-    @rating = @review.ratings.build(rating_params)
-    if @rating.save
-      redirect_to movie_reviews_path(@review.movie), notice: "レビューを評価しました。"
-    else
-    render :new
-    end
+    @rating = @review.ratings.create(rating_params)
+    redirect_to movie_path(@review.movie)
   end
 
   private
 
    def rating_params
-    params.require(:rating).permit(:e_rating, :l_rating, :c_rating)
+    params.require(:rating).permit(:e_rating, :l_rating, :c_rating, :review_id)
    end
    
 end
