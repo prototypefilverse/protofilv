174e271a393c807fbdf0b943241b0cea9c35c740
prototypefilverse <morikawasusumu.b@gmail.com>
2023-02-18 22:48:25 +0900

    Update rating.rb

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -5,5 +5,17 @@ class Rating < ApplicationRecord
   has_many :user_ratings
   has_many :users, through: :user_ratings
 
+  after_create :update_filv
+
+  private
+
+  def update_filv
+    filv = self.review.reviewer.user.filv
+    filv.update(
+    strength: filv.strength + self.e_rating,
+    intelligence: filv.intelligence + self.l_rating,
+    charisma: filv.charisma + self.c_rating
+    )
+  end
   
 end
