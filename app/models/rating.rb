feb20d5c2a8057b447165bdfd64bea89e53a51ee
prototypefilverse <morikawasusumu.b@gmail.com>
2023-02-18 23:46:25 +0900

    Update rating.rb

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -7,6 +7,7 @@ class Rating < ApplicationRecord
 
   after_create :update_filv
   after_destroy :remove_from_filv
+  after_update :update_filv_on_update
 
   private
 
@@ -26,6 +27,18 @@ class Rating < ApplicationRecord
       intelligence: filv.intelligence - self.l_rating,
       charisma: filv.charisma - self.c_rating
     )
+  end
+
+  def update_filv_on_update
+    filv = self.review.reviewer.user.filv
+    prev_e_rating, prev_l_rating, prev_c_rating = self.saved_changes["e_rating"], self.saved_changes["l_rating"], self.saved_changes["c_rating"]
+    if prev_e_rating.present? && prev_l_rating.present? && prev_c_rating.present?
+      filv.update(
+        strength: filv.strength + self.e_rating - prev_e_rating[0],
+        intelligence: filv.intelligence + self.l_rating - prev_l_rating[0],
+        charisma: filv.charisma + self.c_rating - prev_c_rating[0]
+      )
+    end  
   end
 
 end
