6f5e3d87e5166c16a385e890a792c967fda69ab6
prototypefilverse <morikawasusumu.b@gmail.com>
2023-02-18 23:17:24 +0900

    レビューを消すと評価点も消える

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -6,6 +6,7 @@ class Rating < ApplicationRecord
   has_many :users, through: :user_ratings
 
   after_create :update_filv
+  after_destroy :remove_from_filv
 
   private
 
@@ -18,4 +19,13 @@ class Rating < ApplicationRecord
     )
   end
   
+  def remove_from_filv
+    filv = self.review.reviewer.user.filv
+    filv.update(
+      strength: filv.strength - self.e_rating,
+      intelligence: filv.intelligence - self.l_rating,
+      charisma: filv.charisma - self.c_rating
+    )
+  end
+
 end
