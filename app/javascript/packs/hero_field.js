9656096565982bbca3db880cfecccfea41c63594
prototypefilverse <morikawasusumu.b@gmail.com>
2024-01-17 16:50:43 +0900

    Update hero_field.js

--- app/javascript/packs/hero_field.js
+++ app/javascript/packs/hero_field.js
@@ -1,13 +1,14 @@
-// /Users/morikawa/projects/protofilv/app/javascript/hero_field.js
+
 
 document.addEventListener('turbolinks:load', () => {
   const heroAvatar = document.querySelector('.yuusya_image');
-  
+
   if (heroAvatar) {
-    heroAvatar.addEventListener('click', () => {
-      // ここに勇者がフィールドで活躍するためのコードを書きます
-      console.log('勇者が活躍を始めました！');
-      // 例えば、バトルフィールドに遷移するか、何かアクションを起こすなど
+    heroAvatar.addEventListener('click', (e) => {
+      e.preventDefault(); // デフォルトのリンクの動作を防ぎます
+      alert('勇者が活躍を始めました！');
+      // ここに他のアクションやアニメーションを追加できます
     });
   }
 });
+
