4bbb49d02b33359aac26eb2b95d0df0cd3232c69
prototypefilverse <morikawasusumu.b@gmail.com>
2024-01-17 14:39:26 +0900

    Update field.js

--- app/javascript/packs/field.js
+++ app/javascript/packs/field.js
@@ -1,10 +1,10 @@
+import heroImage from 'images/yuusya.png'; // webpackerを使っている場合
+
 document.addEventListener('turbolinks:load', () => {
   const field = document.querySelector('.field-side');
   if (field) {
-    // 勇者が活躍するフィールドに関連するJavaScriptコードをここに配置
-    // 例：勇者のアニメーションやイベントリスナーの設定
     const heroAvatar = document.createElement('img');
-    heroAvatar.src = 'path_to_your_hero_image.png'; // 勇者の画像パスを設定
+    heroAvatar.src = heroImage; // importした画像パスを使用
     heroAvatar.classList.add('hero-avatar');
     field.appendChild(heroAvatar);
 
